44aa71ee80102f7defa4432d4dc0c5b25f7cb8a8
MOHD SABIR <sabiransari01981@gmail.com>
2022-08-07 12:15:31 +0530

    Render The home view page

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,3 +1,4 @@
 module.exports.home =function(req , res ){
-    res.send('<h1>Thsi is Response from the server</h1>');
+   return res.render('home')
+
 }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require('express');
 var app= express();
-
+ 
+app.set('view engine', 'ejs')
+app.set('views','./views')
 
 app.use('/' ,require('./routers'));
 
