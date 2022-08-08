c17762303baa3342540f3ad6767ebe5c76b0ee5f
MOHD SABIR <sabiransari01981@gmail.com>
2022-08-08 13:36:48 +0530

    SETUP LAYOut file and save it here

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express=require('express');
 var app= express();
+const expressLayouts = require('express-ejs-layouts');
  
 app.set('view engine', 'ejs')
 app.set('views','./views')
 
-//handle all requestes comes from '/' path 
-app.use('/' ,require('./routers'));
+//This use t o set the layouts for all EJS files and Always define before routes m/w
+app.use(expressLayouts);
 
+//handle all requestes comes from '/' path
+app.use('/' ,require('./routers'));
 
 app.listen(8000,function(error){
     if(error){console.log(error);}
     console.log("server is running for Sample Project");
 })
+
+
