ec357546afa4d6fec669d1e3122f5a8952d714e7
MOHD SABIR <sabiransari01981@gmail.com>
2022-08-25 13:30:51 +0530

    Adding comments

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -4,8 +4,8 @@ const Post = require('../models/posts');
 
 module.exports.home = function(req ,res ){
     Post.find({})
-    .populate('user')
-    .exec(function(err , data){
+    .populate('user') // used to populate the user which ref by the post
+    .exec(function(err , data){     // exec() is the callback function
         if(err){console.log('Error ',err);}
         return res.render('home' , {
             title:'Home Page',
