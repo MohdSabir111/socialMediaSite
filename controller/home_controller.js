d664086f47cca2ce27f87c8e5a30188f7e27a140
MOHD SABIR <sabiransari01981@gmail.com>
2022-09-15 20:23:08 +0530

    modifiyng homePadeCode using Async Await

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -2,40 +2,62 @@ const { render } = require('ejs');
 const Post = require('../models/posts');
 const User= require('../models/users');
 
+//Using async await
 
+module.exports.home = async function(req ,res ){
 
-module.exports.home = function(req ,res ){
-    Post.find({})
-    .populate('user') // used to populate the user which ref by the post
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err , data){     // exec() is the callback function
-
-         User.find({}, function(err,users){
-            if(err){console.log('Error ',err);}
+    try{
+        let posts = await  Post.find({})
+        .populate('user') // used to populate the user which ref by the post
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+    
+        let users = await User.find({});
         return res.render('home' , {
             title:'Home Page',
-            posts : data,
+            posts : posts,
             all_users : users
+        });
 
-        })
-        
-        })
-    })
+    }catch(err){
+        console.log("Error",err);
+        return;
+
+    }
+
 }
 
 
 
-
-
-// function (err , data){
-//     if(err){ console.log('Error ',err);}
-//     return res.render('home' , {
-//         title:'Home Page',
-//         posts : data
+// module.exports.home = function(req ,res ){
+//     Post.find({})
+//     .populate('user') // used to populate the user which ref by the post
+//     .populate({
+//         path:'comments',
+//         populate:{
+//             path:'user'
+//         }
 //     })
-// })
+//     .exec(function(err , data){     // exec() is the callback function
+
+//          User.find({}, function(err,users){
+//             if(err){console.log('Error ',err);}
+//         return res.render('home' , {
+//             title:'Home Page',
+//             posts : data,
+//             all_users : users
+
+//         })
+        
+//         })
+//     })
+// }
+
+
+
+
+
