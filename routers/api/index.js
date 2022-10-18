200ae55d4d6b13234d247a2535c6cd349b246918
MOHD SABIR <sabiransari01981@gmail.com>
2022-10-18 12:50:06 +0530

    Dir Structure APIs setup

---
+++ routers/api/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+// add roueter function of express 
+const router = express.Router();
+
+module.exports=router;
--- routers/index.js
+++ routers/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //add controller File in Homecont
 const homeCont = require('../controller/home_controller');
 
-console.log("Router is connected to the main Index file");
+
 
 //here we use the HomeCont function which is exported 
 router.get('/',homeCont.home);
@@ -15,6 +15,8 @@ router.use('/users' , require('./users'));  //use()..its a middle ware
 
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
+
+router.use('/api',require('./api'));
 
 
 //Thisis  require to export so  that server can use it 'router'
